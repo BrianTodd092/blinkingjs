1c8f46caa231d687adb302c6f02a527a87f8d4aa
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2023-11-19 11:19:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,20 @@
 
 
 
-  //var d = document.getElementById('blink');
-  var d = document.getElementById('diva');
-//if (d.style.display==='block') {
+  var d = document.getElementById('blink');
+ // var d = document.getElementById('diva');
+if (d.style.display==='block') {
 //if (d.style.color === 'red') {
 
-  if (d.style.backgroundColor === 'red') {
+ // if (d.style.backgroundColor === 'red') {
 
-  //d.style.display = 'none';
+  d.style.display = 'none';
   //d.style.color = 'blue';
-d.style.backgroundColor = 'blue';
-  }else  {
-    //d.style.display = 'block';
+//d.style.backgroundColor = 'blue';
+  } else  {
+    d.style.display = 'block';
     //d.style.color = 'red';
-    d.style.backgroundColor = 'red';
+    //d.style.backgroundColor = 'red';
   }
 }
 setInterval(toggleItem, 1000);
